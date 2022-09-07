efe98c1644a5bef73ba79d37d053c4f5563828f2
Ardaerd <arderd@outlook.com>
2022-09-07 22:01:17 +0300

    draw cube is done

--- Cube.py
+++ Cube.py
@@ -2,8 +2,8 @@ import pygame
 
 class Cube:
     
-    rows = 0
-    width = 0
+    rows = 20
+    width = 500
     
     def __init__(self, start, dx = 1, dy = 0, color = (255,0,0)):
         self.pos = start
@@ -19,4 +19,19 @@ class Cube:
         
     
     def draw(self, surface, eyes=False):
-        pass
+        dis = self.width // self.row
+        
+        i = self.pos[0]
+        j = self.pos[1]
+        
+        pygame.draw.rect(surface, self.color, i*dis + 1, j*dis + 1, dis-2, dis-2)
+        
+        if eyes:
+            centre = dis//2
+            radius = 3
+            circleMiddle = (i*dis+1,j*dis+1,dis-2,dis-2)
+            circleMiddle2 = (i*dis+dis - radius*2,j*dis+8)
+            pygame.draw.circle(surface, (0,0,0),circleMiddle, radius)
+            pygame.draw.circle(surface, (0,0,0),circleMiddle2, radius)
+        
+        
