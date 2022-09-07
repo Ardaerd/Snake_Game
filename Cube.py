504e6efc1c94c7ccd8840059c26856dc0b8c8b1f
Ardaerd <arderd@outlook.com>
2022-09-07 21:46:38 +0300

    init method of the cube is done

--- Cube.py
+++ Cube.py
@@ -6,7 +6,11 @@ class Cube:
     width = 0
     
     def __init__(self, start, dx = 1, dy = 0, color = (255,0,0)):
-        pass
+        self.pos = start
+        self.dx = 1
+        self.dy = 0
+        self.color = color
+
     
     def move(self, dx, dy):
         pass
